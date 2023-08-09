efdc47aee63dc3c12848e35d24084b39e153aed2
himanshu nigam <hnigam743@gmail.com>
2023-08-09 20:18:38 +0530

    first commit

--- about.js
+++ about.js
@@ -1,30 +1,29 @@
 let string = "";
-let buttons = document.querySelectorAll(".button");
-
+let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button) => {
       button.addEventListener('click', (e) => {
-           if(e.target.innerHTML === "="){
-                  string = eval(string.replace("%", "/100"))
+            if(e.target.innerHTML == "=") {
+                  string = eval(string.replace('%', '/100'))
                   document.querySelector('input').value = string
-           }
-           else if (e.target.innerHTML === "="){
-            string = eval(string);
-            document.querySelector('input').value = string
-           }
-           else if (e.target.innerHTML === "c"){
-            string = ''
-            document.querySelector('input').value = string
-           }
-           else if (e.target.innerHTML === "x"){
-            string = string.substring(0, string.length-1)
-            document.querySelector('input').value = string
-           }
-           
-           
-           
-            else{console.log(e.target)
-            string = string + e.target.innerHTML;
-            document.querySelector('input').value = string
-      }
+            }
+            else if(e.target.innerHTML == '='){
+                  string = eval(string)
+                  document.querySelector('input').value = string
+
+            }
+            else if(e.target.innerHTML == 'c'){
+                  string = ""
+                  document.querySelector('input').value = string
+
+            }
+            else if(e.target.innerHTML == "x"){
+                  string = string.toString().slice(0,-1)
+                  document.querySelector('input').value = string
+                 }
+            else{
+                  console.log(e.target)
+                  string = string + e.target.innerHTML;
+                  document.querySelector('input').value = string
+            }
       })
 })
